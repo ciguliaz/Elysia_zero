15da073ac4f9dad3e0d9b07cd0678ea9b819cc5c
ciguliaz <ckazzgd@gmail.com>
2025-02-06 23:28:31 +0700

    change auth middleware - throw error -> http respond

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -10,9 +10,10 @@ export const authenticate = new Elysia()
 		})
 	)
 	//! Middleware that runs before request handling, ensuring the Authorization header exists
-	.onBeforeHandle(({ headers }) => {
+	.onBeforeHandle(({ headers, set }) => {
 		if (!headers.authorization) {
-			throw new Error("Authorization header is required"); // Prevents requests without Authorization header
+			set.status = 401;
+			return { error: "Authorization header is required" }; // Prevents requests without Authorization header
 		}
 	})
 	// Derives user data by verifying the JWT token
@@ -20,15 +21,23 @@ export const authenticate = new Elysia()
 		async ({
 			jwt,
 			headers,
+			set,
 		}: {
 			jwt: { verify: (token: string) => Promise<any> }; // Ensures `jwt` has a `verify` function
 			headers: { authorization?: string }; // `authorization` header is optional but checked earlier
+			set: any;
 		}) => {
-			const token = headers.authorization?.split(" ")[1]; // Extracts token from "Bearer <token>"
-			if (!token) return { error: "Unauthorized" }; // Rejects requests without a token
+			const token = headers.authorization?.split(" ")[1]; //! Extracts token from "Bearer <token>"
+			if (!token) {
+				set.status = 401; //Unauthorized
+				return { error: "Unauthorized" }; // Rejects requests without a token
+			}
 
 			const user = await jwt.verify(token); //! Verifies the JWT token
-			if (!user) return { error: "Invalid token" }; // Rejects invalid tokens
+			if (!user) {
+				set.status = 403; //Forbidden
+				return { error: "Invalid token" }; // Rejects invalid tokens
+			}
 
 			return { user }; // Returns the decoded user data
 		}
