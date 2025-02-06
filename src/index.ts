8a44e3410d94b3716207be9306a31dcb1e633bfd
ciguliaz <ckazzgd@gmail.com>
2025-02-07 00:55:43 +0700

    add swagger lib
    for easy moderate api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv';
 import { Elysia } from 'elysia';
 import mongoose from 'mongoose';
+import { swagger } from '@elysiajs/swagger';
+
 import authRoutes from './routes/auth';
+import serverRoutes from './routes/server';
 import chat from './routes/chat';
-import { logDaR, logIdR } from './utils/logFormat';
+import { logDaR, logIdR, logLiR } from './utils/logFormat';
 import { timestamps, logWithTime } from './utils/log';
 
 dotenv.config();
@@ -15,8 +18,11 @@ mongoose.connect(process.env.MONGO_URI!)
 	.catch(err => console.error('MongoDB Error:', err));
 
 app
+	.use(swagger())
 	.use(authRoutes)
+	.use(serverRoutes)
 	.use(chat)
 	.listen(3000);
 
-logWithTime('Server is running on http://localhost:3000');
+logWithTime('Server is running on ' + logLiR('http://localhost:3000'));
+logWithTime('Swagger server is running on '+logLiR('http://localhost:3000/swagger'));
