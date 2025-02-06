a5febc792692140fe335b4f15bc40f8f8446f4c1
ciguliaz <ckazzgd@gmail.com>
2025-02-07 01:03:22 +0700

    update user schema attribure
    - ignore, no conflict data

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true, index: true },
     password: { type: String, required: true },
-    joinedServers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Server' }]
+    servers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Server' }]
 }, { timestamps: true });
 
 export default mongoose.model('User', UserSchema);
