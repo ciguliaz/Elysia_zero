1c712d64977d6196b9a478d2b1db75aab6203afd
ciguliaz <ckazzgd@gmail.com>
2025-02-08 22:12:23 +0700

    tested server route modules (done)
    - checkpoint

--- src/routes/server.ts
+++ src/routes/server.ts
@@ -4,28 +4,28 @@ import Server from '../models/Server';
 import User from '../models/User';
 
 import * as log from '../utils/log';
+import type { ObjectId } from 'mongoose';
 
 interface UserType {
-	id: any;
+	id: ObjectId;
 }
 
 const serverRoutes =
-	// authenticate
 	new Elysia().use(authenticate)
 
 		//! Create a new server --------------
 		.post('/server', async ({ body, user }: { body: any; user: UserType }) => {  //* Tested - GOOD
-
 			log.stamp(log.PathR() + 'Creating Server')
 
 			if (!user) {
 				log.stamp(log.PathR() + `${log.ErR('Failed getting user')}: ${user}`)
 				return { error: "Unauthorized post/server" };  //  Ensure user is defined
 			}
-			const { name, description } = body as { name: string; description?: string }
 
+			const { name, description } = body as { name: string; description?: string }
 			const server = await Server.create({ name, description, owner: user.id, member: [user.id] })
 			await User.findByIdAndUpdate(user.id, { $push: { servers: server._id } })
+
 			log.stamp(log.PathR() + `Server Created: ${log.Raw(name, 96)} \n`)
 			return { success: true, server }
 		}, {
@@ -36,19 +36,24 @@ const serverRoutes =
 		.post('/server/join', async ({ body, user }: { body: any; user: UserType }) => {   //* Tested - GOOD
 			log.stamp(log.PathR() + 'Joining Server')
 			const { serverId } = body as { serverId: string }
+
 			const server = await Server.findById(serverId)
 			if (!server) return { error: 'Server not found' }
+			const thisUser = await User.findById(user.id);
+			if (!thisUser) return { error: 'User not found' }
 
 			// Add
-			if (!server.members.includes(user.id)) {
+			if (!server.members.map(e => e.toString()).includes(user.id.toString())) {
 				// Update both schema
 				await Server.findByIdAndUpdate(serverId, { $push: { members: user.id } });
 				await User.findByIdAndUpdate(user.id, { $push: { servers: server._id } });
+
+				log.stamp(log.PathR() + `${log.Raw(thisUser.username, 96)} Joined Server ${log.Raw(server.name, 96)} `)
 				return { success: true, message: "Joined the server" };
 			} else {
+				log.stamp(log.PathR() + `${log.Raw(thisUser.username, 96)} Is Already In The Server ${log.Raw(server.name, 96)} `)
 				return { success: false, message: "Already in the server" };
 			}
-
 		}, {
 			body: t.Object({ serverId: t.String() })
 		})
@@ -56,17 +61,25 @@ const serverRoutes =
 		//! Leave -------------------
 		.post("/server/leave", async ({ body, user }: { body: any; user: UserType }) => {   //* Tested - GOOD
 			log.stamp(log.PathR() + 'Leaving Server')
-
 			const { serverId } = body as { serverId: string };
 
-			const server = await Server.findById(serverId);
-			if (!server) return { error: "Server not found" };
+			const server = await Server.findById(serverId)
+			if (!server) return { error: 'Server not found' }
+			const thisUser = await User.findById(user.id);
+			if (!thisUser) return { error: 'User not found' }
 
-			// Remove user from the server
-			await Server.findByIdAndUpdate(serverId, { $pull: { members: user.id } });
-			await User.findByIdAndUpdate(user.id, { $pull: { servers: serverId } });
+			// Remove
+			if (server.members.map(e => e.toString()).includes(user.id.toString())) {
+				// Update both schema
+				await Server.findByIdAndUpdate(serverId, { $pull: { members: user.id } });
+				await User.findByIdAndUpdate(user.id, { $pull: { servers: server._id } });
 
-			return { success: true, message: "Left the server" };
+				log.stamp(log.PathR() + `${log.Raw(thisUser.username, 96)} Left The Server ${log.Raw(server.name, 96)} `)
+				return { success: true, message: "Left the server" };
+			} else {
+				log.stamp(log.PathR() + `${log.Raw(thisUser.username, 96)} Is Not In The Server ${log.Raw(server.name, 96)} `)
+				return { success: false, message: "Not in the server" };
+			}
 		}, {
 			body: t.Object({ serverId: t.String() })
 		})
@@ -74,8 +87,11 @@ const serverRoutes =
 		//! Get server this user is part of --------------
 		.get('/servers', async ({ user }: { user: UserType }) => {
 			log.stamp(log.PathR() + 'Getting Servers list')
+			const thisUser = await User.findById(user.id);
+			if (!thisUser) return { error: "User not found" }
+			log.stamp(log.PathR() + `Requested by ${log.Raw(thisUser.username, 96)}`)
 
-			const servers = await Server.find({ member: user.id })
+			const servers = await Server.find({ members: user.id.toString() })
 			console.log(servers)
 			return { success: true, servers }
 		})
@@ -85,12 +101,22 @@ const serverRoutes =
 		.delete('/servers/purge', async ({ body, set }) => { //* Tested - GOOD
 			log.stamp(log.PathR() + `Purging Servers`)
 			const { name } = body as { name: string };
-			const result = await Server.deleteMany({ name: name, _id: { $ne: '67a63d62a32648fa87fd17f2' } });
-			log.stamp(log.PathR() + `Purged ${log.IdR(result.deletedCount.toString())} Servers`)
 
-			if (result.deletedCount > 0) {
-				return { success: true, message: `Deleted ${result.deletedCount} servers with name "${name}".` };
+			const deletedServers = await Server.find({ name: name, _id: { $ne: '67a63d62a32648fa87fd17f2' } }).select('_id');
+
+			const deleteResult = await Server.deleteMany({ name: name, _id: { $ne: '67a63d62a32648fa87fd17f2' } })
+			// log.stamp(log.PathR() + `Purged ${ log.IdR(deleteResult.deletedCount.toString()) } Servers`)
+
+			if (deleteResult.deletedCount > 0) {
+				const deletedServerIds = deletedServers.map(server => server._id);
+				const updatedUser = await User.updateMany(
+					{ servers: { $in: deletedServerIds } },  // Find users who have these servers
+					{ $pull: { servers: { $in: deletedServerIds } } }  // Remove them from the `servers` field
+				);
+				log.stamp(log.PathR() + `Purged ${log.IdR(deleteResult.deletedCount.toString())} Servers; Updated ${log.IdR(updatedUser.modifiedCount.toString())} Users`)
+				return { success: true, message: `Deleted ${deleteResult.deletedCount} servers with name "${name}".` };
 			} else {
+				log.stamp(log.PathR() + `Purged ${log.IdR(deleteResult.deletedCount.toString())} Servers`)
 				set.status = 404;
 				return { success: false, message: `No servers found with name "${name}".` };
 			}
