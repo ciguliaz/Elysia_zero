74fa36c86b2c87bff1eb1049cc45e466b3305f66
ciguliaz <ckazzgd@gmail.com>
2025-03-04 09:09:50 +0700

    fix channel schema to storage chat

--- src/models/Channel.ts
+++ src/models/Channel.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 const ChannelSchema = new mongoose.Schema({
-	name: { type: String, required: true },
-	server: { type: mongoose.Schema.Types.ObjectId, ref: 'Server', required: true, index: true },
-	type: { type: String, enum: ['text'], default: 'text' } // For now, only text channels
+	name: { type: String},
+	server: { type: mongoose.Schema.Types.ObjectId, ref: 'Server' },
+	type: { type: String, enum: ['dm', 'text', 'voice', 'announce'], default: 'text' }, // For now, only text channels
+	messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }]
 }, { timestamps: true });
 
 export default mongoose.model('Channel', ChannelSchema);
